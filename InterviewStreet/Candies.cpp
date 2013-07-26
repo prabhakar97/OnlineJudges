80cf0be22e891b4d93a8483b49b897057ce5d1c9
Prabhakar <prabhakar97@gmail.com>
2013-07-26 23:52:55 +0530

    Fixed spacing in InterviewStreet problems

--- InterviewStreet/Candies.cpp
+++ InterviewStreet/Candies.cpp
@@ -5,106 +5,106 @@
 using namespace std;
 
 map<int, int>* findIncreasingSequences(vector<int> v) {
-        map<int, int> *res = new map<int, int>;
-        if(!v.size())
-                return NULL;
-        if(v.size()==1){
-                (*res)[0] = 1;
-                return res;
-        }
-        int length = 1, i;
-        for(i=1; i<v.size(); i++) {
-                if(i == v.size()-1) {
-                        if(v[i]>v[i-1])
-                                (*res)[i] = length+1;
-                        else {
-                                (*res)[i-1] = length;
-                                (*res)[i] = 1;
-                        }
-                }
-                else if(v[i]>v[i-1])
-                        length++;
-                else {
-                        (*res)[i-1] = length;
-                        length = 1;
-                }
-        }
+    map<int, int> *res = new map<int, int>;
+    if(!v.size())
+        return NULL;
+    if(v.size()==1){
+        (*res)[0] = 1;
         return res;
+    }
+    int length = 1, i;
+    for(i=1; i<v.size(); i++) {
+        if(i == v.size()-1) {
+            if(v[i]>v[i-1])
+                (*res)[i] = length+1;
+            else {
+                (*res)[i-1] = length;
+                (*res)[i] = 1;
+            }
+        }
+        else if(v[i]>v[i-1])
+            length++;
+        else {
+            (*res)[i-1] = length;
+            length = 1;
+        }
+    }
+    return res;
 }
 map<int, int>* findDecreasingSequences(vector<int> v) {
-        map<int, int> *res = new map<int, int>;
-        if(!v.size())
-                return NULL;
-        if(v.size()==1){
-                (*res)[0] = 1;
-                return res;
-        }
-        int length = 1, i;
-        for(i=1; i<v.size(); i++) {
-                if(i == v.size()-1) {
-                        if(v[i]<v[i-1])
-                                (*res)[i] = length+1;
-                        else {
-                                (*res)[i-1] = length;
-                                (*res)[i] = 1;
-                        }
-                }
-                else if(v[i]<v[i-1])
-                        length++;
-                else {
-                        (*res)[i-1] = length;
-                        length = 1;
-                }
-        }
+    map<int, int> *res = new map<int, int>;
+    if(!v.size())
+        return NULL;
+    if(v.size()==1){
+        (*res)[0] = 1;
         return res;
+    }
+    int length = 1, i;
+    for(i=1; i<v.size(); i++) {
+        if(i == v.size()-1) {
+            if(v[i]<v[i-1])
+                (*res)[i] = length+1;
+            else {
+                (*res)[i-1] = length;
+                (*res)[i] = 1;
+            }
+        }
+        else if(v[i]<v[i-1])
+            length++;
+        else {
+            (*res)[i-1] = length;
+            length = 1;
+        }
+    }
+    return res;
 }
 
 int main() {
-        int n, m;
-        cin>>n;
-        m=n;
-        map<int, int> *increasingList;
-        map<int, int> *decreasingList;
-        vector<int> v;
-        int temp;
-        while(n--) {
-                cin>>temp;
-                v.push_back(temp);
+    int n, m;
+    cin>>n;
+    m=n;
+    map<int, int> *increasingList;
+    map<int, int> *decreasingList;
+    vector<int> v;
+    int temp;
+    while(n--) {
+        cin>>temp;
+        v.push_back(temp);
+    }
+    increasingList = findIncreasingSequences(v);
+    decreasingList = findDecreasingSequences(v);
+    vector<int> candyVector;
+    candyVector.resize(m);
+    for(map<int, int>::iterator it = (*increasingList).begin(); it != (*increasingList).end(); it++) {
+        //cout<<it->first<<" - "<<it->second<<endl;
+        if(it->second > 1) {
+            int index = it->first;
+            for(int i=it->second; i>0; i--) {
+                candyVector[index]=i;
+                index--;
+            }
         }
-        increasingList = findIncreasingSequences(v);
-        decreasingList = findDecreasingSequences(v);
-        vector<int> candyVector;
-        candyVector.resize(m);
-        for(map<int, int>::iterator it = (*increasingList).begin(); it != (*increasingList).end(); it++) {
-                //cout<<it->first<<" - "<<it->second<<endl;
-                if(it->second > 1) {
-                        int index = it->first;
-                        for(int i=it->second; i>0; i--) {
-                                candyVector[index]=i;
-                                index--;
-                        }
-                }
-        }
+    }
 
-        for(map<int, int>::iterator it = (*decreasingList).begin(); it != (*decreasingList).end(); it++) {
-                //cout<<it->first<<" - "<<it->second<<endl;
-                if(it->second > 1) {
-                        int index = it->first;
-                        for(int i=1; i<=it->second; i++) {
-                                if(candyVector[index]>i)
-                                        break;
-                                candyVector[index]=i;
-                                index--;
-                        }
-                }
+    for(map<int, int>::iterator it = (*decreasingList).begin(); it != (*decreasingList).end(); it++) {
+        //cout<<it->first<<" - "<<it->second<<endl;
+        if(it->second > 1) {
+            int index = it->first;
+            for(int i=1; i<=it->second; i++) {
+                if(candyVector[index]>i)
+                    break;
+                candyVector[index]=i;
+                index--;
+            }
         }
-        int result=0;
-        for(int i=0; i<candyVector.size(); i++) {
-                if(candyVector[i]==0)
-                        candyVector[i]=1;
-                result+=candyVector[i];
-                //cout<<candyVector[i]<<" ";
-        }
-        cout<<result;
-        return 0;
+    }
+    int result=0;
+    for(int i=0; i<candyVector.size(); i++) {
+        if(candyVector[i]==0)
+            candyVector[i]=1;
+        result+=candyVector[i];
+        //cout<<candyVector[i]<<" ";
+    }
+    cout<<result;
+    return 0;
 }
--- InterviewStreet/KingdomConnectivity.cpp
+++ InterviewStreet/KingdomConnectivity.cpp
@@ -7,89 +7,89 @@
 using namespace std;
 
 enum colors_t {
-        WHITE,
-        GRAY,
-        BLACK
+    WHITE,
+    GRAY,
+    BLACK
 };
 
 class Node {
-        int id_;
-        list<Node*> predecessors;
-        colors_t color_;
-        public:
-        Node() {
-                color_ = WHITE;
-        }
-        int getId() {
-                return id_;
-        }
-        void setId(int id) {
-                id_ = id;
-        }
-        list<Node*> getPredecessors() {
-                return predecessors;
-        }
-        void addPredecessor(Node *node) {
-                predecessors.push_back(node);
-        }
-        void setColor(colors_t color) {
-                color_ = color;
-        }
-        bool operator<(const Node &input) {
-                return (this->id_ < input.id_);
-        }
+    int id_;
+    list<Node*> predecessors;
+    colors_t color_;
+    public:
+    Node() {
+        color_ = WHITE;
+    }
+    int getId() {
+        return id_;
+    }
+    void setId(int id) {
+        id_ = id;
+    }
+    list<Node*> getPredecessors() {
+        return predecessors;
+    }
+    void addPredecessor(Node *node) {
+        predecessors.push_back(node);
+    }
+    void setColor(colors_t color) {
+        color_ = color;
+    }
+    bool operator<(const Node &input) {
+        return (this->id_ < input.id_);
+    }
 };
 
 class Edge {
-        Node *dest_;
-        int weight_;
-        public:
-        Edge() {
-                Edge(NULL);
-        }
-        Edge(Node *dest) {
-                Edge(dest, 1);
-        }
-        Edge(Node *dest, int weight) {
-                dest_ = dest;
-                weight_ = weight;
-        }
-        int getWeight() {
-                return weight_;
-        }
-        Node *getDestNode() {
-                return dest_;
-        }
-        bool operator<(Edge &in) {
-                return (this->dest_->getId() < in.dest_->getId());
-        }
+    Node *dest_;
+    int weight_;
+    public:
+    Edge() {
+        Edge(NULL);
+    }
+    Edge(Node *dest) {
+        Edge(dest, 1);
+    }
+    Edge(Node *dest, int weight) {
+        dest_ = dest;
+        weight_ = weight;
+    }
+    int getWeight() {
+        return weight_;
+    }
+    Node *getDestNode() {
+        return dest_;
+    }
+    bool operator<(Edge &in) {
+        return (this->dest_->getId() < in.dest_->getId());
+    }
 };
 
 // This graph stores its edges in an adjacency list structure. Each list is sorted and requires log(n) search
 class Graph {
-        vector< multiset<Edge>* > edgeList;
-        vector<Node *> nodeList;
-        public:
-        void init(int numNodes) {
-                edgeList.resize(numNodes);
-                nodeList.resize(numNodes);
-                for(int i=0; i<numNodes; i++) {
-                        edgeList.push_back(new multiset<Edge>);
-                        Node *node = new Node;
-                        node->setId(i);
-                        nodeList.push_back(node);
-                }
+    vector< multiset<Edge>* > edgeList;
+    vector<Node *> nodeList;
+    public:
+    void init(int numNodes) {
+        edgeList.resize(numNodes);
+        nodeList.resize(numNodes);
+        for(int i=0; i<numNodes; i++) {
+            edgeList.push_back(new multiset<Edge>);
+            Node *node = new Node;
+            node->setId(i);
+            nodeList.push_back(node);
         }
-        void addEdge(int source, int dest) {
-                Edge edge(nodeList.at(dest-1));
-                edgeList.at(source-1)->insert(edge);
-        }
+    }
+    void addEdge(int source, int dest) {
+        Edge edge(nodeList.at(dest-1));
+        edgeList.at(source-1)->insert(edge);
+    }
 };
 
 string solve(Graph *g, int source, int dest) {
-        return NULL;
+    return NULL;
 }
 
 int main() {
-        return 0;
+    return 0;
 }
