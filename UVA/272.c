e20854468e7625adbeec24c7e55ef0ddc888a9d4
Prabhakar <prabhakar97@gmail.com>
2013-07-26 23:49:27 +0530

    Fixed tabs in all UVA programs

--- UVA/272.c
+++ UVA/272.c
@@ -5,22 +5,22 @@
 #define false 0
 
 int main() {
-	int toggle = false;
-	char ch;
-	while((ch = getchar()) != EOF) {
-			if(ch != '"') {
-					putchar(ch);
-					continue;
-			}
-			if(toggle) {
-					printf("''");
-					toggle = false;
-			}
-			else {
-					printf("``");
-					toggle = true;
-			}
-	}
-	return 0;
+    int toggle = false;
+    char ch;
+    while((ch = getchar()) != EOF) {
+        if(ch != '"') {
+            putchar(ch);
+            continue;
+        }
+        if(toggle) {
+            printf("''");
+            toggle = false;
+        }
+        else {
+            printf("``");
+            toggle = true;
+        }
+    }
+    return 0;
 }
 
